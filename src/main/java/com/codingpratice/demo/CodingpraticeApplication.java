a7f1b76d0951ca8b88a0e321e5115cef02248407
parkjieun87 <dksxlzhd0807@naver.com>
2024-01-16 22:36:21 +0900

    first commit

---
+++ src/main/java/com/codingpratice/demo/CodingpraticeApplication.java
@@ -0,0 +1,13 @@
+package com.codingpratice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingpraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingpraticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingpratice/demo/CodingpraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingpratice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingpraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
