07763eeec6104e88947b50730a8d7949b78c80eb
parkjieun87 <dksxlzhd0807@naver.com>
2024-01-17 10:57:10 +0900

    숫자의 합 구하기

--- src/main/java/com/codingpratice/day1/number.java
+++ src/main/java/com/codingpratice/day1/number.java
@@ -2,20 +2,24 @@ package com.codingpratice.day1;
 
 import java.util.Scanner;
 
-public class 숫자의합 {
+public class number {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		
 		int N = sc.nextInt();
 		
 		String sNum = sc.next();
+		
 		char[] cNum = sNum.toCharArray();
+		
 		int sum = 0;
+		
 		for(int i=0; i<cNum.length; i++) {
 			sum += cNum[i] - '0';
 		}
 		System.out.println(sum);
+		
 	}
-
 }
